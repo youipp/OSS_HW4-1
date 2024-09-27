b834fb32b2d3706ddc0af2399dacfa60c93c5239
21900300 <dmltjd1008@handong.ac.kr>
2024-09-27 11:09:34 +0900

    Hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <Hello />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import './Hello.css';
+
+function Hello(){
+    return (
+       <h1 id='Hello'> hello world !!! Hello.js </h1> 
+    );
+    
+}
+
+export default Hello;
